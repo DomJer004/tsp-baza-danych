c86282c658c9de8d4a0b7dbd604f025d17878290
DomJer004 <dominik.jero1@gmail.com>
2025-12-29 14:17:10 +0100

    Update app.py

--- pythonProject3/app.py
+++ pythonProject3/app.py
@@ -565,3 +565,4 @@ elif opcja == "Młoda Ekstraklasa":
     df = load_data("me.csv")
     df = prepare_dataframe_with_flags(df, 'kraj' if df is not None and 'kraj' in df.columns else 'narodowość')
     st.dataframe(df, use_container_width=True, hide_index=True, column_config={"Flaga": st.column_config.ImageColumn("Flaga", width="small")})
+
